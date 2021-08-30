82754626c83934996e2fec80732da584684d1d8c
Santiago Marcano <marcanoblog@gmail.com>
2021-08-30 12:35:12 +0300

    minor styling on lists

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -3,17 +3,8 @@ import Structure from "layouts/Dashboard";
 import { $t } from "store/TranslationsContext";
 import useCollection from "hooks/useCollection";
 import Loader from "components/Loader";
-import Overlay from "components/Overlay";
-import PageForm from "components/PageForm";
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  UnorderedList,
-} from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, UnorderedList } from "@chakra-ui/react";
 import { Module, PageModule } from "interfaces/declarations";
-import { PageProvider } from "store/PageContext";
 import { collection } from "firebase/firestore";
 import { db } from "utils/firebase";
 import { RouteComponentProps, Link } from "@reach/router";
@@ -35,30 +26,33 @@ export default function Pages({}: PageProps) {
               <Button colorScheme="blue">{$t("NEW_PAGE")}</Button>
             </Link>
           </Flex>
-          <Divider my="5" />
-          <UnorderedList>
+          <Divider my={5} />
+          <UnorderedList m={0}>
             {pages?.docs.map((page: Module) => (
-              <Link key={page.id} to={`/dashboard/pages/${page.id}`}>
-                <Box
-                  boxShadow="sm"
-                  width="100%"
-                  background="white"
-                  borderWidth="1px"
-                  borderRadius="lg"
-                  overflow="hidden"
-                  padding="3"
-                  cursor="pointer"
-                  transition="background 0.5s"
-                  _hover={{
-                    background: "gray.200",
-                  }}
-                >
-                  <Flex justifyContent="space-between" alignItems="center">
-                    <div>{page.id}</div>
-                    <FiArrowRight />
-                  </Flex>
-                </Box>
-              </Link>
+              <React.Fragment key={page.id}>
+                <Link key={page.id} to={`/dashboard/pages/${page.id}`}>
+                  <Box
+                    boxShadow="sm"
+                    width="100%"
+                    background="white"
+                    borderWidth="1px"
+                    borderRadius="lg"
+                    overflow="hidden"
+                    padding="3"
+                    cursor="pointer"
+                    transition="background 0.5s"
+                    _hover={{
+                      background: "gray.200",
+                    }}
+                  >
+                    <Flex justifyContent="space-between" alignItems="center">
+                      <div>{page.id}</div>
+                      <FiArrowRight />
+                    </Flex>
+                  </Box>
+                </Link>
+                <Divider my={2} />
+              </React.Fragment>
             ))}
           </UnorderedList>
         </>
